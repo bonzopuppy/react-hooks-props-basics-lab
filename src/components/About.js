ef2c593efa4ed627f004aaf927aabdf114d47a41
David Baden <davebaden@gmail.com>
2023-11-20 13:25:39 -0800

    pass 2/3 tests

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* If a prop of bio is passed to this component, display in p, if not don't display the p */}
+      {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links links={props.links} />
+
     </div>
   );
 }
